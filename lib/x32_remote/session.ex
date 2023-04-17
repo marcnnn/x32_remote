0975f1935905cb15d5f741bb2e1728794678e530
Adrian Irving-Beer <wisq@wisq.net>
2023-04-17 18:17:41 -0400

    Handle /node replies correctly.

--- lib/x32_remote/session.ex
+++ lib/x32_remote/session.ex
@@ -70,8 +70,24 @@ defmodule X32Remote.Session do
 
   defp handle_one_event(%Message{} = msg, state) do
     Logger.debug("<<< #{inspect(msg)}")
-    {reply_to, replies} = Map.pop(state.replies, msg.path, [])
+    {reply_to, replies} = Map.pop(state.replies, msg.path |> reply_lookup_path(), [])
     Enum.each(reply_to, &GenStage.reply(&1, msg.args))
     %State{state | replies: replies}
   end
+
+  # For some reason, requests with path "/node" are answered by replies with path "node".
+  # This reverses that.
+  #
+  # Note that this is currently only intended for ease of use while exploring the API.
+  # There are zero API functions that rely on "/node" commands.
+  #
+  # If you're using this in your own code, keep in mind that if you issue a lot
+  # of different "/node" requests in rapid succession, it's very likely that
+  # you'll start getting responses that don't match your requests.
+  #
+  # To prevent this, it should be possible to look at the contents of the
+  # reply, and ensure that the first word at least contains the query string.
+  # But since (again) this is just for exploratory use, this will do for now.
+  defp reply_lookup_path("node"), do: "/node"
+  defp reply_lookup_path(path), do: path
 end
