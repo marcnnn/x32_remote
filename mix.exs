2906bd65fc43acb9a5f3a9d35c5de3539bd8c4c8
Adrian Irving-Beer <wisq@wisq.net>
2023-04-20 02:02:32 -0400

    Migrate to published ex_osc.

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule X32Remote.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:gen_stage, "~> 1.2.1"},
-      {:ex_osc, github: "wisq/ex_osc", tag: "main"},
+      {:ex_osc, "~> 0.1.1"},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:ex_git_test, github: "wisq/ex_git_test", tag: "main", only: :test, runtime: false}
     ]
