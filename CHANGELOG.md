e6b71a8675c9112ea2f74e78a0eb5ae05f25e9ac
Adrian Irving-Beer <wisq@wisq.net>
2023-04-25 00:48:16 -0400

    Bump and update changelog.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v0.1.1
+
+* Add the ability to load scenes
+* Add future types for cues, snippets, and channel presets
+* Some builder fixes to handle cases where modules or functions are missing docs
+
 ## v0.1.0
 
 * Initial release.
--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule X32Remote.MixProject do
   def project do
     [
       app: :x32_remote,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
